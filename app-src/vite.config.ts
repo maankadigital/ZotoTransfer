1f32bf3ba68e078bc65d240e687a95e4dacfc547
Trmaa <trmaa@example.com>
2026-07-05 23:25:11 +0300

    config: optimize code splitting and standard dist directory output for vercel

--- app-src/vite.config.ts
+++ app-src/vite.config.ts
@@ -6,7 +6,23 @@ import { resolve } from 'path'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: resolve(__dirname, '../transfer web'),
+    outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
+    chunkSizeWarningLimit: 1200,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('@supabase') || id.includes('supabase')) {
+              return 'supabase';
+            }
+            if (id.includes('react')) {
+              return 'react-vendor';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 })
